519074bef393d95f60a243b3b3756b40626b88ff
Jon Alaniz <jonalaniz@gmail.com>
2024-11-30 00:20:03 -0600

    Initial Commit

---
+++ Kami/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kami
+//
+//  Created by Jon Alaniz on 11/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
