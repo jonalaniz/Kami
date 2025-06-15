dc7627566aa5a7727537694a4099a257cdf5bde9
Jon Alaniz <jonalaniz@gmail.com>
2025-06-15 06:49:41 -0500

    Reorganized folder structure

---
+++ Kami/iOS/TableViewCells/ConversationPreviewCell.swift
@@ -0,0 +1,127 @@
+//
+//  ConversationPreviewCell.swift
+//  Kami
+//
+//  Created by Jon Alaniz on 12/1/24.
+//
+
+import UIKit
+
+/// A custom table view cell that displays a preview of a conversation.
+///
+/// The `ConversationPreviewCell` is designed to show key information about a conversation,
+/// such as the sender, subject, preview text, and a visual indicator for its status.
+class ConversationPreviewCell: BaseTableViewCell {
+
+    /// The reuse identifier for this cell.
+    static let reuseIdentifier = "ConversationPreviewCell"
+
+    /// A label to display the name of the sender.
+    private var senderLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .headerText
+        label.font = .preferredFont(forTextStyle: .headline)
+        label.numberOfLines = 1
+        return label
+    }()
+
+    /// A label to display the timestamp of the conversation.
+    private var timeStampLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .subheadline)
+        label.textAlignment = .right
+        label.numberOfLines = 1
+        return label
+    }()
+
+    /// A label to display the subject of the conversation.
+    private var subjectLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .headerText
+        label.font = .preferredFont(forTextStyle: .body)
+        label.numberOfLines = 1
+        return label
+    }()
+
+    /// A label to display a preview of the conversation's content.
+    private var previewLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .text
+        label.font = .preferredFont(forTextStyle: .caption1)
+        label.textColor = .secondaryLabel
+        label.numberOfLines = 1
+        return label
+    }()
+
+    /// A vertical stack view to organize the labels.
+    private var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 4
+
+        return stackView
+    }()
+
+    /// Configures the initial appearance and behavior of the cell.
+    ///
+    /// - Sets the accessory type to a disclosure indicator.
+    /// - Adds a custom selected background view.
+    override func setupViews() {
+        accessoryType = .disclosureIndicator
+        selectedBackgroundView = SelectedView()
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.addArrangedSubview(senderLabel)
+        stackView.addArrangedSubview(subjectLabel)
+        stackView.addArrangedSubview(previewLabel)
+
+        contentView.addSubview(stackView)
+    }
+
+    /// Applies constraints to the views inside of the cell.
+    override func setupConstraints() {
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 14),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
+        ])
+    }
+
+    /// Configures the cell with the given conversation data.
+    ///
+    /// - Parameter conversation: A `ConversationPreview` object containing
+    ///   the sender, subject, preview, and status information.
+    func configure(with conversation: ConversationPreview) {
+        senderLabel.text = conversation.customer?.name()
+        subjectLabel.text = conversation.subject
+        previewLabel.text = conversation.preview
+
+        // Here we configure the bkcolor
+        guard let status = ConversationStatus(rawValue: conversation.status)
+        else {
+            backgroundColor = .inactiveCell
+            return
+        }
+
+        setBackgroundColor(for: status)
+    }
+
+    /// Updates the cell's background color based on the conversation status.
+    ///
+    /// - Parameter status: The `ConversationStatus` value representing the conversation's current state.
+    private func setBackgroundColor(for status: ConversationStatus) {
+        switch status {
+        case .active: backgroundColor = .activeCell
+        default: backgroundColor = .inactiveCell
+        }
+    }
+}
+
+/// Enum representing the possible statuses of a conversation.
+enum ConversationStatus: String {
+    case active
+    case closed
+    case pending
+    case spam
+}
---
+++ Kami/iOS/ViewControllers/BaseTableViewController.swift
@@ -0,0 +1,87 @@
+//
+//  BaseTableViewController.swift
+//  Kami
+//
+//  Created by Jon Alaniz on 12/1/24.
+//
+
+import UIKit
+
+/// A reusable base class for table view controllers.
+///
+/// `BaseTableViewController` simplifies the creation of table view controllers by
+/// managing common configurations such as navigation bar appearance, toolbar setup,
+/// table view initialization, and layout constraints. Subclass this to implement custom table views.
+class BaseTableViewController: UIViewController {
+    // MARK: - Properties
+
+    weak var coordinator: MainCoordinator?
+    var tableView: UITableView!
+    var dataSource: UITableViewDataSource?
+    var delegate: UITableViewDelegate?
+    var titleText: String?
+    var tableStyle: UITableView.Style = .plain
+    var toolbarColor: UIColor = .subHeaderToolbar
+    var toolbarTint: UIColor = .iconsTexts
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupToolbar()
+        setupTableView()
+        registerCells()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        guard let indexPath = tableView.indexPathForSelectedRow
+        else { return }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    // MARK: - Configuration
+
+    func setupView() {
+        if let titleText = titleText { title = titleText }
+        let attributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.titleTextAttributes = attributes
+        navigationController?.navigationBar.largeTitleTextAttributes = attributes
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.barTintColor = .background
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    func setupToolbar() {
+        let toolbar = navigationController?.toolbar
+        toolbar?.setColors(background: toolbarColor, tint: toolbarTint)
+        navigationController?.setToolbarHidden(false, animated: true)
+    }
+
+    /// Initializes and configures the table view, setting its delegate, data source, and layout constraints.
+    private func setupTableView() {
+        tableView = UITableView(frame: .zero, style: tableStyle)
+        tableView.backgroundColor = .background
+        tableView.delegate = delegate
+        tableView.dataSource = dataSource
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(tableView)
+        activateFullScreenConstraints(for: tableView)
+    }
+
+    func registerCells() {}
+
+    // MARK: - Helper Functions
+
+    private func activateFullScreenConstraints(for subview: UIView) {
+        NSLayoutConstraint.activate([
+            subview.topAnchor.constraint(equalTo: view.topAnchor),
+            subview.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            subview.leftAnchor.constraint(equalTo: view.leftAnchor),
+            subview.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ])
+    }
+}
